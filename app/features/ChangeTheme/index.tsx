a582b055a953257e83920774a414d555fd36ed5f
leandro.bernardi <leandro.bernardi@consultr.net>
2024-03-21 13:08:59 -0300

    update

--- app/features/ChangeTheme/index.tsx
+++ app/features/ChangeTheme/index.tsx
@@ -5,20 +5,22 @@ import { useThemeSlice } from 'stores'
 export function ChangeTheme() {
     const { theme, setTheme } = useThemeSlice((state) => state)
     return (
-        <>
-            <button onClick={() => setTheme('light')} className="btn btn-primary">
-                Change Theme light
-            </button>
-            <button onClick={() => setTheme('dark')} className="btn btn-primary">
-                Change Theme dark
-            </button>
-            <button onClick={() => setTheme('lofi')} className="btn btn-primary">
-                Change Theme lofi
-            </button>
-            <button onClick={() => setTheme('luxury')} className="btn btn-primary">
-                Change Theme luxury
-            </button>
+        <div className="flex	 flex-col">
+            <div className="flex justify-between gap-2">
+                <button onClick={() => setTheme('light')} className="btn btn-primary">
+                    Change Theme light
+                </button>
+                <button onClick={() => setTheme('dark')} className="btn btn-primary">
+                    Change Theme dark
+                </button>
+                <button onClick={() => setTheme('lofi')} className="btn btn-primary">
+                    Change Theme lofi
+                </button>
+                <button onClick={() => setTheme('luxury')} className="btn btn-primary">
+                    Change Theme luxury
+                </button>
+            </div>
             <p>Current Theme: {theme}</p>
-        </>
+        </div>
     )
 }
